872b6f04e1d6d142c0865fe1f6c9e71919ca89b5
mora <493487492@qq.com>
2025-07-10 20:47:58 +0800

    mysql,rocketmq,redis

---
+++ docker/mysql/slave/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+# Start mysqld in the background
+docker-entrypoint.sh mysqld &
+
+# Wait for the slave's mysqld to be ready
+until mysqladmin ping -h"localhost" -u"root" -p"${MYSQL_ROOT_PASSWORD}" --silent; do
+    echo 'waiting for slave mysqld to be ready...'
+    sleep 2
+done
+
+# Wait for the master's mysqld to be ready
+until mysqladmin ping -h"mysql-master" -u"${MYSQL_REPLICATION_USER}" -p"${MYSQL_REPLICATION_PASSWORD}" --silent; do
+    echo 'waiting for master mysqld to be ready...'
+    sleep 2
+done
+
+echo "Slave and Master are ready, configuring replication."
+
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "
+CHANGE MASTER TO
+  MASTER_HOST='mysql-master',
+  MASTER_USER='${MYSQL_REPLICATION_USER}',
+  MASTER_PASSWORD='${MYSQL_REPLICATION_PASSWORD}',
+  MASTER_AUTO_POSITION=1;
+START SLAVE;
+"
+
+echo "Replication configured."
+
+# Bring the background mysqld process to the foreground
+wait 
